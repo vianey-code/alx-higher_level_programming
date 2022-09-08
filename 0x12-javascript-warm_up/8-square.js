7377ae542d048b34640ad619f50783703334bc79
vianey <vianeynkem@gmail.com>
2022-09-07 17:32:15 -0700

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const entry = process.argv;
+
+const size = Math.floor(entry[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
