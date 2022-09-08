86931b07e1bd01918b8c70338b1b9ac4f0ee2f21
vianey <vianeynkem@gmail.com>
2022-09-07 17:22:09 -0700

    a script that prints 3 lines by arrays and loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const listOfStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < listOfStrings.length; i++) {
+  console.log(listOfStrings[i]);
+}
